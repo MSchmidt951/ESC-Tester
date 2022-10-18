9136f4fcabc72734e17591d61f592b494832822a
MSchmidt951 <m.c.schmidt951@gmail.com>
2022-10-18 15:55:27 +0100

    Update radio settings

--- ESC-Tester/ESC-Tester.ino
+++ ESC-Tester/ESC-Tester.ino
@@ -90,9 +90,9 @@ void setup() {
 
   //Set up radio
   radio.begin();
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_1MBPS);
+  radio.setRadiation(RF24_PA_MAX, RF24_2MBPS);
   radio.setChannel(124);
+  radio.setPayloadSize(7);
   
   //Open pipe
   radio.openWritingPipe(addresses[1]);
